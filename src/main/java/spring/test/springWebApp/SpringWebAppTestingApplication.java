b4aee7ea122e16bd3e75bf3c3abb2e24ea985a4b
marianavanucci <marianavanucciandrade@gmail.com>
2021-09-19 16:26:33 -0300

    initial commit

---
+++ src/main/java/spring/test/springWebApp/SpringWebAppTestingApplication.java
@@ -0,0 +1,13 @@
+package spring.test.springWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/test/springWebApp/SpringWebAppTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.test.springWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAppTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
